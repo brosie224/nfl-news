56b849cf50c567ce350d81dd8d7be2f3355f9178
Brian Rosengarten <barosengarten@gmail.com>
2019-01-08 00:57:17 +0000

    first commit

---
+++ lib/nfl/news.rb
@@ -0,0 +1,8 @@
+require "nfl/news/version"
+
+module Nfl
+  module News
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/nfl/news/version.rb
@@ -0,0 +1,5 @@
+module Nfl
+  module News
+    VERSION = "0.1.0"
+  end
+end
